2ad01675f366a80798ff2b525ccff346392daee6
bhokumar <bhoopendra.akgec@gmail.com>
2024-01-02 23:42:44 +0530

    Adding changes

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  swiperModules = [IonicSlides];
   constructor() { }
 
   ngOnInit() {
