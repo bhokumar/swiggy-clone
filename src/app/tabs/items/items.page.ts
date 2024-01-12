3735559b72a9a9b2bee76c80a2d96aba35a2a536
bhokumar <bhoopendra.akgec@gmail.com>
2024-01-12 23:36:29 +0530

    Adding paramMap

--- src/app/tabs/items/items.page.ts
+++ src/app/tabs/items/items.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      // redirect
+      console.log(paramMap);
+    });
   }
 
 }
